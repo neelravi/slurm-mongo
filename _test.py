7e4f39a5428261ac0a6fce1c57166bcfe25b1af7
Ravindra Shinde <neelravi@gmail.com>
2023-04-14 14:27:34 +0200

    tests verified

--- _test.py
+++ _test.py
@@ -1,6 +1,6 @@
 import unittest
 from slurm_usage_to_mongodb import main
-
+import subprocess
 
 class TestSlurmMongo(unittest.TestCase):
 
@@ -12,9 +12,21 @@ class TestSlurmMongo(unittest.TestCase):
         port = 27017
         database = "Slurm"
         collection = "Usage"
-        args = [start, end, host, connection, port, database, collection]
-        self.assertIsNone(main(args))
+        args = [start, end, host, "--connection", connection, "--port", str(port), "--database", database, "--collection", collection]
 
+        # Construct the command as a list of strings
+        command = ["python", "-m", "slurm_usage_to_mongodb", start, end, host, "--connection", connection, "--port", str(port), "--database", database, "--collection", collection]
+        print("command: ", command)
+
+        # Run the command as a subprocess and capture the output
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        print ("result: ", result)
+        print ("result.stdout: ", result.stdout)
+        print ("result.stderr: ", result.stderr)
+        # Check that the subprocess ran successfully with no errors
+        self.assertEqual(result.returncode, 0)
+        self.assertEqual(result.stderr, b"")  # Check that there were no errors
 
 if __name__ == '__main__':
     unittest.main()
